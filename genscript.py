8e5d10fe7620750ad1e11753ba4b024b22d31d39
Taylor Monacelli <taylormonacelli@gmail.com>
2019-03-24 07:59:55 -0700

    Now you can specify frequency as formula
    I don't know how many seconds in the day without
    looking it up. Now I don't have to.

--- genscript.py
+++ genscript.py
@@ -1,19 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
+import jinja2
 import yaml
-from jinja2 import Template
 
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
-tpl_logger = Template("""{# jinja2 -#}
+
+def eval_to_int(x):
+    """ example: convert 24*60 to 1440 """
+    if isinstance(x, str):
+        return eval(x)
+    else:
+        return x
+
+
+jinja2.filters.FILTERS['eval_to_int'] = eval_to_int
+
+tpl_logger = jinja2.Template("""{# jinja2 -#}
 /usr/bin/logger -is "Starting '$HOME/Library/Application Support/{{ label }}/{{ program }}' from $HOME/Library/LaunchAgents/{{ label }}.plist"
 """)
 
 
-tpl_installer = Template("""{# jinja2 -#}
+tpl_installer = jinja2.Template("""{# jinja2 -#}
 #!/bin/bash
 
 set -e
@@ -40,12 +51,28 @@ cat <<__eot__ >$HOME/Library/LaunchAgents/{{ label }}.plist
     <key>Program</key>
     <string>$HOME/Library/Application Support/{{ label }}/{{ program }}</string>
 
+    {% if EnvironmentVariables %}
+    <key>EnvironmentVariables</key>
+    <dict>
+        {% for var in EnvironmentVariables -%}
+        {% for key, value in var.items() -%}
+    	<key>{{key}}</key>
+    	<string>{{value}}</string>
+        {%- endfor %}
+        {%- endfor %}
+    </dict>
+    {%- endif %}
+
+    <key>WorkingDirectory</key>
+    <string>$HOME/Library/Application Support/{{ label }}</string>
+
     <!-- great for debug since it runs as soon as we do launchctl load -->
     <key>RunAtLoad</key>
     <true/>
 
     <key>StandardErrorPath</key>
     <string>$HOME/Library/Logs/{{ label }}/{{ label }}.err</string>
+
     <key>StandardOutPath</key>
     <string>$HOME/Library/Logs/{{ label }}/{{ label }}.out</string>
 
@@ -54,7 +81,7 @@ cat <<__eot__ >$HOME/Library/LaunchAgents/{{ label }}.plist
     <integer>19</integer>
 
     <key>StartInterval</key>
-    <integer>{{ minute_frequency * 60 }}</integer>
+    <integer>{{ freq|eval_to_int * 60 }}</integer>
 
   </dict>
 </plist>
@@ -66,12 +93,13 @@ chmod +x "$HOME/Library/Application Support/{{ label }}/{{ program }}"
 # debug
 launchctl unload $HOME/Library/LaunchAgents/{{ label }}.plist
 launchctl load $HOME/Library/LaunchAgents/{{ label }}.plist
-launchctl list {{ label }}
 launchctl list | grep {{ label }}
+tail $HOME/Library/Logs/{{ label }}/{{ label }}.{err,out}
+launchctl list {{ label }}
 ls -la $HOME/Library/Logs/{{ label }}/*
 ls -la $HOME/Library/LaunchAgents/{{ label }}.plist
 cat "$HOME/Library/Application Support/{{ label }}/{{ program }}"
-cat $HOME/Library/Logs/{{ label }}/{{ label }}.{err,out}
+cat "$HOME/Library/LaunchAgents/{{ label }}.plist"
 COMMENTBLOCK
 
 # reminder for how to cleanup/abort this plist
@@ -86,6 +114,16 @@ ABORT_UNLOAD_AND_CLEANUP
 
 documents = """
 ---
+label: net.taylorm.launcha.show-urls-for-recent-homebrews
+program: main.py
+minute_frequency: 24*60
+script: |
+ #!/bin/sh
+ {{ logger }}
+ ~/pdev/taylormonacelli/show-urls-for-recent-homebrews/main.py
+EnvironmentVariables:
+- PATH: /bin:/usr/bin:/usr/local/bin
+---
 label: net.taylorm.launcha.conditionally-pause-backblaze
 program: pause-backup
 minute_frequency: 15
@@ -96,7 +134,7 @@ script: |
 ---
 label: net.taylorm.launcha.gcloudcomponentsupdate
 program: updater
-minute_frequency: 60
+minute_frequency: 24*60
 script: |
  #!/bin/sh
  {{ logger }}
@@ -112,9 +150,12 @@ script: |
 """
 
 for dct in yaml.load_all(documents):
-    tpl_script = Template(dct['script'])
+    tpl_script = jinja2.Template(dct['script'])
     label = dct['label']
     genscript = "{}.sh".format(label)
+
     with open(genscript, 'w') as file_h:
         file_h.write(tpl_installer.render(
-            {**dct, 'script': tpl_script.render(logger=tpl_logger.render(dct))}))
+            {**dct,
+             'freq': dct['minute_frequency'],
+             'script': tpl_script.render(logger=tpl_logger.render(dct))}))
